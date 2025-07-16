a2ab0610cc5e89cc347bf7e5b56bde7c392e5cba
Alberto Codutti <alberto.codutti@eurotech.com>
2025-07-16 16:34:08 +0200

    :recycle: [Core] Improved type Converter and their usages
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- message/internal/src/test/java/org/eclipse/kapua/message/internal/KapuaPositionTest.java
+++ message/internal/src/test/java/org/eclipse/kapua/message/internal/KapuaPositionTest.java
@@ -14,7 +14,6 @@ package org.eclipse.kapua.message.internal;
 
 import org.eclipse.kapua.commons.util.xml.XmlUtil;
 import org.eclipse.kapua.message.KapuaPosition;
-import org.eclipse.kapua.model.xml.DateXmlAdapter;
 import org.eclipse.kapua.qa.markers.junit.JUnitTests;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,7 +30,7 @@ public class KapuaPositionTest {
 
     private static final String NEWLINE = System.lineSeparator();
 
-    private static ZonedDateTime referenceDate = ZonedDateTime.of(2017, 1, 18, 12, 10, 46, 238000000, ZoneId.of(DateXmlAdapter.TIME_ZONE_UTC));
+    private static ZonedDateTime referenceDate = ZonedDateTime.of(2017, 1, 18, 12, 10, 46, 238000000, ZoneId.of("UTC"));
 
     private static final String POSITION_DISPLAY_STR = "^altitude=.*" +
             "~~heading=.*" +
--- service/api/src/main/java/org/eclipse/kapua/model/type/ByteArrayConverter.java
+++ service/api/src/main/java/org/eclipse/kapua/model/type/ByteArrayConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2022 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2025 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,14 +12,32 @@
  *******************************************************************************/
 package org.eclipse.kapua.model.type;
 
+import com.google.common.base.Strings;
+
 import javax.xml.bind.DatatypeConverter;
 
+/**
+ * Converts a {@link Byte}[] to {@link String} and vice-versa
+ *
+ * @since 1.0.0
+ */
 public class ByteArrayConverter {
 
     private ByteArrayConverter() {
     }
 
+    /**
+     * Converts the given {@link Byte}[] to a Base64 {@link String}
+     *
+     * @param byteArray The {@link Byte}[] to convert, can be {@code null}
+     * @return The Base64 representation.
+     * @since 1.0.0
+     */
     public static String toString(Byte[] byteArray) {
+        if (byteArray == null) {
+            return null;
+        }
+
         byte[] unboxedByteArray = new byte[byteArray.length];
 
         for (int i = 0; i < byteArray.length; i++) {
@@ -29,11 +47,33 @@ public class ByteArrayConverter {
         return toString(unboxedByteArray);
     }
 
+    /**
+     * Converts the given {@code byte}[] to a Base64 {@link String}
+     *
+     * @param byteArray The {@code byte}[] to convert, can be {@code null}
+     * @return The Base64 representation.
+     * @since 1.0.0
+     */
     public static String toString(byte[] byteArray) {
+        if (byteArray == null) {
+            return null;
+        }
+
         return DatatypeConverter.printBase64Binary(byteArray);
     }
 
+    /**
+     * Converts the given Base64 {@link String} into a {@code byte}[]
+     *
+     * @param base64 The Base64 {@link String}, can be {@code null}.
+     * @return The converted {@code byte[]}
+     * @since 1.0.0
+     */
     public static byte[] fromString(String base64) {
+        if (Strings.isNullOrEmpty(base64)) {
+            return null;
+        }
+
         return DatatypeConverter.parseBase64Binary(base64);
     }
 }
