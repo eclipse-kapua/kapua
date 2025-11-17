d68b3a06c28fcb574d64f07eab9dd7f5fec426a6
angelo.andreussi <angelo.andreussi@gmail.com>
2025-11-17 12:07:19 +0100

    :sparkles: rewrote fetchUserClaims to not use a "brute-force" approach (enumerating all possible domains/actions)

--- service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/shiro/AuthorizationServiceImpl.java
+++ service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/shiro/AuthorizationServiceImpl.java
@@ -12,11 +12,11 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.authorization.shiro;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -26,12 +26,17 @@ import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.KapuaUnauthenticatedException;
 import org.eclipse.kapua.commons.security.KapuaSecurityUtils;
 import org.eclipse.kapua.commons.security.KapuaSession;
+import org.eclipse.kapua.model.domain.Actions;
 import org.eclipse.kapua.model.domain.Domain;
 import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.service.authentication.AuthenticationService;
+import org.eclipse.kapua.service.authentication.token.LoginInfo;
 import org.eclipse.kapua.service.authorization.AuthorizationService;
+import org.eclipse.kapua.service.authorization.access.AccessPermission;
 import org.eclipse.kapua.service.authorization.exception.SubjectUnauthorizedException;
 import org.eclipse.kapua.service.authorization.permission.Permission;
 import org.eclipse.kapua.service.authorization.permission.PermissionFactory;
+import org.eclipse.kapua.service.authorization.role.RolePermission;
 
 /**
  * {@link AuthorizationService} implementation.
@@ -44,15 +49,18 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     private final PermissionFactory permissionFactory;
     private final Set<Domain> knownDomains;
     private final PermissionMapper permissionMapper;
+    private final AuthenticationService authenticationService;
 
     @Inject
     public AuthorizationServiceImpl(
             PermissionFactory permissionFactory,
             Set<Domain> knownDomains,
-            PermissionMapper permissionMapper) {
+            PermissionMapper permissionMapper,
+            AuthenticationService authenticationService) {
         this.permissionFactory = permissionFactory;
         this.knownDomains = knownDomains;
         this.permissionMapper = permissionMapper;
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -75,24 +83,61 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     }
 
     @Override
-    public Set<String> fetchUserClaims(KapuaId inScope) {
-        final Set<String> claims = knownDomains.stream()
-                .flatMap(domain -> {
-                    final Stream<String> domainClaims = domain.getActions()
-                            .stream()
-                            .filter(action -> {
-                                try {
-                                    final Permission permission = permissionFactory.newPermission(domain.getName(), action, inScope);
-                                    return this.isPermitted(permission);
-                                } catch (KapuaException e) {
-                                    return false;
-                                }
-                            })
-                            .map(action -> String.format("%s:%s", domain.getName(), action));
-                    return domainClaims;
-                })
-                .collect(Collectors.toSet());
+    public Set<String> fetchUserClaims(KapuaId inScope) throws KapuaException {
+        LoginInfo loginInfo = authenticationService.getLoginInfo();
+        //Retrieve all permissions from both AccessPermissions and RolePermissions
+        Set<AccessPermission> accessPermissions = loginInfo.getAccessPermission();
+        Set<RolePermission> accessRolePermissions = loginInfo.getRolePermission();
+        Set<Permission> permissions = accessPermissions.stream().map(ap -> (Permission) ap.getPermission()).collect(Collectors.toSet());
+        Set<Permission> permissionsRole = accessRolePermissions.stream().map(ap -> (Permission) ap.getPermission()).collect(Collectors.toSet());
+        permissions.addAll(permissionsRole);
+
+        Set<String> claims = new java.util.HashSet<>();
+        List<String> domains = new ArrayList<>();
+        List<Actions> actions = new ArrayList<>();
+
+        for (Permission p : permissions) {
+            resolveDomain(p, domains); //Resolve domains considering the "*" a possibility
+            for (String d : domains) {
+                resolveAction(p, d, actions); //Resolve actions considering the "*" a possibility
+                for (Actions a : actions) {
+                    try {
+                        final Permission permission = permissionFactory.newPermission(d, a, inScope, p.getGroupId()); //GroupId could be null (no group) or a specific group
+                        if (this.isPermitted(permission)) {
+                            claims.add(String.format("%s:%s", d, a));
+                        }
+                    } catch (KapuaException e) {
+                        // Ignore
+                    }
+                }
+                actions.clear();
+            }
+            domains.clear();
+        }
         return claims;
+    }
+
+    private void resolveDomain(Permission p, List<String> domains) {
+        if (p.getDomain() != null) { //Specific domain
+            domains.add(p.getDomain());
+        } else { //All domains
+            for (Domain d : knownDomains) {
+                domains.add(d.getName());
+            }
+        }
+    }
+
+    private void resolveAction(Permission p, String d, List<Actions> actions) {
+        if (p.getAction() != null) { //Specific Action
+            actions.add(p.getAction());
+        } else { //All Actions
+            Domain targetDomain = knownDomains.stream()
+                    .filter(domain -> d.equals(domain.getName()))
+                    .findFirst()
+                    .orElse(null);
+
+            actions.addAll(targetDomain.getActions());
+        }
     }
 
     @Override
